a6512c8b54572a3b9a8d9af668b10ff57f9f6e7f
AndrusovN <andrusov.n@gmail.com>
2020-11-29 21:30:14 +0300

    drawing with bugs

--- app/src/main/java/kennel/pheditor/drawing/drawing.kt
+++ app/src/main/java/kennel/pheditor/drawing/drawing.kt
@@ -1,5 +1,6 @@
 package kennel.pheditor.drawing
 
+import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
@@ -7,10 +8,7 @@ import android.graphics.Paint
 import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.MotionEvent
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -42,7 +40,7 @@ class DrawingFragment : Fragment() {
         val bton: ImageView = binding.imageView
         //bmap = (bton.getDrawable() as BitmapDrawable).bitmap
         binding.imageView.setImageDrawable(GlobalVars.image)
-        bton?.setOnTouchListener { v, event ->  onTouchEvent(event)}
+        binding.imageView?.setOnTouchListener { v, event ->  onTouchEvent(event)}
 
         return binding.root
     }
@@ -53,22 +51,52 @@ class DrawingFragment : Fragment() {
         val paint: Paint = Paint()
         paint.setColor(Color.GREEN) // установим зеленый цвет
         paint.setStyle(Paint.Style.FILL)
-        paint.strokeWidth = 10.toFloat()
-        if (e.action == 0) {
-            lastX = e.x - bton.x
-            lastY = e.y - bton.y// - bton.height / 4
-        }
+
 
         val bitmap = DrawableHelper.drawableToBitmap(GlobalVars.image)!!.copy(Bitmap.Config.ARGB_8888, true)
 
+        paint.strokeWidth = 10.toFloat() * bitmap.width / binding.imageView.width
+
+        var ratioBitmap = bitmap.width.toFloat() / bitmap.height.toFloat()
+        var ratioImage = binding.imageView.width.toFloat() / binding.imageView.height.toFloat()
+
+        var x = 0f
+        var y = 0f
+        var width = 0f
+        var height = 0f
+
+        if(ratioBitmap > ratioImage){
+            x = 0f
+            y = (binding.imageView.height - bitmap.height * bitmap.width.toFloat() / binding.imageView.width) / 2
+
+            width = binding.imageView.width.toFloat()
+            height = binding.imageView.height * bitmap.width.toFloat() / binding.imageView.width
+
+        } else{
+            x = (binding.imageView.width - bitmap.width * bitmap.height.toFloat() / binding.imageView.height) / 2
+            y = 0f
+
+            width = binding.imageView.width * bitmap.height.toFloat() / binding.imageView.height
+            height = binding.imageView.height.toFloat()
+        }
+
+        val finishX = (e.getX() - x) * bitmap.width.toFloat() / width
+        val finishY = (e.getY() - y) * bitmap.height.toFloat() / height
+
+        if (e.action == 0) {
+            lastX = finishX
+            lastY = finishY
+        }
+
         val cvas = Canvas(bitmap)
-        cvas.drawLine(lastX, lastY,e.x - bton.x, e.y - bton.y, paint)
 
-        
-        Log.i("TEST", e.x.toString() + " " + e.y.toString() + " " + lastX.toString() + " " + lastY.toString())
+        cvas.drawLine(lastX, lastY,finishX, finishY, paint)
 
-        lastX = e.x - bton.x
-        lastY = e.y - bton.y
+        Log.i("TEST",(binding.root.width / binding.imageView.width).toString() )
+        Log.i("TEST", finishX.toString() + " " + finishY.toString() + " " + e.getX().toString() + " " + e.getY().toString())
+
+        lastX = finishX
+        lastY = finishY
         //cvas.drawCircle(e.x - bton.x, e.y - bton.y - bton.height / 2, 10.toFloat(), paint)
         //GlobalVars.image!!.draw(cvas)
 
