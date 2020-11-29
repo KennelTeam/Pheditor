9c42ef766cc774b36bfba5234677a78fde07953d
Alexey <alexey.nebolsin.jr@gmail.com>
2020-11-29 11:20:22 +0300

    Initial commit

---
+++ app/src/main/java/kennel/pheditor/MainActivity.kt
@@ -0,0 +1,11 @@
+package kennel.pheditor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
